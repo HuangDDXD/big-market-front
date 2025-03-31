f9bea8fd354115366b4853779a61cbca342268b9
HuangDDXD <149939518+HuangDDXD@users.noreply.github.com>
2025-03-31 23:28:57 +0800

    feat: 应用接口对接抽奖页面

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,17 +1,35 @@
 // 请求地址
-const apiHostUrl = process.env.API_HOST_URL ? process.env.API_HOST_URL : "https://m1.apifoxmock.com/m1/4633300-4283744-default";
+import {strategy} from "sharp";
+
+const apiHostUrl = process.env.API_HOST_URL ? process.env.API_HOST_URL : "http://localhost:8091";
+
+/**
+ * 装配抽奖
+ * @param strategyId 策略ID
+ */
+export const strategyArmory = (strategyId?: number) => {
+    return fetch(`${apiHostUrl}/api/v1/raffle/strategy_armory?strategyId=${strategyId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
 
 /**
  * 查询抽奖奖品列表
  * @param strategyId 策略ID
  */
-export const queryRaffleAwardList = (strategyId: number) => {
+export const queryRaffleAwardList = (strategyId: number | undefined) => {
     try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list?strategyId=${strategyId}`, {
-            method: 'get',
+        return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list`, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
-            }
+            },
+            body: JSON.stringify({
+                strategyId: strategyId
+            })
         });
     } catch (error) {
         return fetch("{\n" +
@@ -24,11 +42,14 @@ export const queryRaffleAwardList = (strategyId: number) => {
 
 export const randomRaffle = (strategyId: number) => {
     try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle?strategyId=${strategyId}`, {
-            method: 'get',
+        return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle`, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
-            }
+            },
+            body: JSON.stringify({
+                strategyId: strategyId
+            })
         })
     } catch (error) {
         return fetch("{\n" +
